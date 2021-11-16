1fb7bde2ddd0ad9b4c85fcf2c5a168467b043db2
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-16 19:49:39 +0200

    improved check for get-rows route.

--- backend/blueprints/data.py
+++ backend/blueprints/data.py
@@ -1,4 +1,5 @@
 from os import path
+from flask.json import jsonify
 import pandas as pd
 import flask
 from flask import request
@@ -31,16 +32,30 @@ def export_csv(fileName, dirPath):
     return flask.jsonify(success=True, message="File saved successfully")
 
 
+# get rows in range [start:end) ~ start inclusive, end exclusive.
 @data.get('/data/rows-between/<start>/<end>')
 def rows_between(start, end):
-    start = max(int(start), 0)
-    end = min(int(end), sv.dataFrame.shape[0])
+    try:
+        start = int(start)
+        end = int(end)
+    except Exception as e:
+        return jsonify(success=False, message=str(e))
+
+    if end < start:
+        return flask.jsonify(success=False, message="End cannot be less than Start")
+
+    if start < 0:
+        return flask.jsonify(success=False, message="Start cannot be less than 0")
+
+    if end > sv.dataFrame.shape[0]:
+        return flask.jsonify(success=False, message="End cannot be greater than rows number")
 
     df_json = sv.dataFrame[start:end].to_json(orient='split')
 
     return flask.jsonify(success=True, message="Success", dataframe=df_json)
 
 
+# helper route
 @data.get('/printhead')
 def printhead():
     print(sv.dataFrame.head())
@@ -50,15 +65,18 @@ def printhead():
 # drop columns by labels
 @data.route('/data/drop/columns', methods=['POST'])
 def drop_columns():
+    # get data from request
     data = json.loads(request.data)
     labels = data["labels"]
 
+    # create valid labels set. (label valid if dataFrame contains it)
     dropLabels = set()
     colLabels = sv.dataFrame.columns
     for label in labels:
         if label in colLabels:
             dropLabels.add(label)
 
+    # drop columns
     sv.dataFrame = sv.dataFrame.drop(columns=dropLabels)
 
     return flask.jsonify(success=True, message="Success")
@@ -67,15 +85,18 @@ def drop_columns():
 # drop rows by index
 @data.route('/data/drop/rows', methods=["POST"])
 def drop_rows():
+    # get data from request
     data = json.loads(request.data)
     index = data["index"]
 
+    # create valid index set. (index valid if between 0:nRows)
     dropIndex = set()
     nRows = sv.dataFrame.shape[0]
     for idx in index:
         if idx >= 0 and idx < nRows:
             dropIndex.add(idx)
 
+    # drop rows
     sv.dataFrame = sv.dataFrame.drop(
         sv.dataFrame.index[list(dropIndex)], axis=0)
 
