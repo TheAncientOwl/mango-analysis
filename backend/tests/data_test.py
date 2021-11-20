c7d4dd4ade447e9665ecbbdf6b964cf8f2915a89
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-20 12:22:27 +0200

    code refactoring.

--- backend/tests/data_test.py
+++ backend/tests/data_test.py
@@ -1,74 +1,44 @@
-import unittest
-
-import pandas
-
-import server_data as sv
-import server
-app = server.app
-
-working_dir = 'C:/Users/TheAncientOwl/Code/data-analysis-tool/backend'
-success = 'success'
-message = 'message'
-app_json = 'application/json'
+from test_base import *
 
 
-class DataTest(unittest.TestCase):
+class DataTest(TestBase):
     def test_import_csv(self):
-        tester = app.test_client(self)
-
         # unexisting csv file
-        response = tester.get(
-            f'/data/import/csv/{working_dir}/Datedasd.csv')
+        response = self.client.get(
+            f'/data/import/csv/{tokens.working_dir}/Datedasd.csv')
         json_data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, app_json)
-        self.assertTrue(success in json_data)
-        self.assertTrue(message in json_data)
-        self.assertFalse(json_data[success])
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
 
         # existing csv file
-        response = tester.get(
-            f'/data/import/csv/{working_dir}/Date.csv')
+        response = self.client.get(
+            f'/data/import/csv/{tokens.working_dir}/Date.csv')
         json_data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, app_json)
-        self.assertTrue(success in json_data)
-        self.assertTrue(message in json_data)
-        self.assertTrue(json_data[success])
+        self.assertBasics(response, json_data)
+        self.assertTrue(json_data[tokens.success])
 
     def test_export_csv(self):
-        tester = app.test_client(self)
-
         # directory does not exist
-        response = tester.get(
-            f'/data/export/csv/name/DateExported.csv/path/{working_dir}/non-existing/wdkjdjkjw')
+        response = self.client.get(
+            f'/data/export/csv/name/DateExported.csv/path/{tokens.working_dir}/non-existing/wdkjdjkjw')
         json_data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, app_json)
-        self.assertTrue(success in json_data)
-        self.assertTrue(message in json_data)
-        self.assertFalse(json_data[success])
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
 
         # nothing to save
-        response = tester.get(
-            f'/data/export/csv/name/DateExported.csv/path/{working_dir}')
+        response = self.client.get(
+            f'/data/export/csv/name/DateExported.csv/path/{tokens.working_dir}')
         json_data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, app_json)
-        self.assertTrue(success in json_data)
-        self.assertTrue(message in json_data)
-        self.assertFalse(json_data[success])
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
 
         # save the data
-        sv.dataFrame = pandas.read_csv(f'{working_dir}/Date.csv')
-        response = tester.get(
-            f'/data/export/csv/name/DateExported.csv/path/{working_dir}')
+        self.readDataFrame()
+        response = self.client.get(
+            f'/data/export/csv/name/DateExported.csv/path/{tokens.working_dir}')
         json_data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, app_json)
-        self.assertTrue(success in json_data)
-        self.assertTrue(message in json_data)
-        self.assertTrue(json_data[success])
+        self.assertBasics(response, json_data)
+        self.assertTrue(json_data[tokens.success])
 
 
 if __name__ == '__main__':
---
+++ backend/tests/test_base.py
@@ -0,0 +1,53 @@
+import unittest
+import pandas
+import server
+import server_data as sv
+
+
+class tokens:
+    working_dir = 'C:/Users/TheAncientOwl/Code/data-analysis-tool/backend'
+    success = 'success'
+    message = 'message'
+    app_json = 'application/json'
+
+
+class TestBase(unittest.TestCase):
+    # test case set-up + tear-down
+    def setUp(self):
+        self.emptyDataFrame()
+        self.client = server.app.test_client(self)
+
+    def tearDown(self):
+        pass
+
+    # data-frame read + empty
+    def readDataFrame():
+        sv.dataFrame = pandas.read_csv(f'{tokens.working_dir}/Date.csv')
+
+    def emptyDataFrame():
+        sv.dataFrame = pandas.DataFrame()
+
+    # custom asserts
+    def assertHasSuccess(self, json):
+        self.assertTrue(tokens.success in json,
+                        msg="Json response missing 'success' key.")
+
+    def assertHasMessage(self, json):
+        self.assertTrue(tokens.message in json,
+                        msg="Json response missing 'message' key.")
+
+    # check if status_code = given status code(default 200) and content_type is app/json
+    def assertHasResponseOk(self, response, status_code=200):
+        self.assertEqual(response.status_code, status_code,
+                         msg=f'Response status code should be {status_code}, not {response.status_code}.')
+        self.assertEqual(response.content_type, tokens.app_json,
+                         msg=f'Response content type should be {tokens.app_json}, not {response.content_type}')
+
+    # asserts basic response format:
+    #   code: 200;
+    #   content-type: app/json;
+    #   has success and message keys in json response
+    def assertBasics(self, response, json, status_code=200):
+        self.assertHasResponseOk(response, status_code)
+        self.assertHasSuccess(json)
+        self.assertHasMessage(json)
