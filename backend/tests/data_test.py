69c13d679114d7327a6b36e6a8bc5e8854c17837
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-20 02:15:04 +0200

    added tests for import csv route.

--- backend/tests/data_test.py
+++ backend/tests/data_test.py
@@ -1,18 +1,33 @@
 import unittest
+
 import server as sv
 
 
 class DataTest(unittest.TestCase):
-    # * Check if response is 200
-    def test_index(self):
+    def test_import_csv(self):
         tester = sv.app.test_client(self)
 
-        def test_route(self, route):
-            response = tester.get(route)
-            self.assertEqual(response.status_code, 400)
+        # test unexisting csv file
+        response = tester.get(
+            "/data/import/csv/C:/Users/TheAncientOwl/Code/data-analysis-tool/backend/Datedasd.csv")
+        json_data = response.get_json()
 
-        test_route(
-            self, "/data/import/csv/C:/Users/TheAncientOwl/Code/data-analysis-tool/backend/Date.csv")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "application/json")
+        self.assertTrue('success' in json_data)
+        self.assertTrue('message' in json_data)
+        self.assertEqual(False, json_data['success'])
+
+        # test existing csv file
+        response = tester.get(
+            "/data/import/csv/C:/Users/TheAncientOwl/Code/data-analysis-tool/backend/Date.csv")
+        json_data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "application/json")
+        self.assertTrue('success' in json_data)
+        self.assertTrue('message' in json_data)
+        self.assertEqual(True, json_data['success'])
 
 
 if __name__ == "__main__":
