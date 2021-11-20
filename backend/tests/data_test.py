188509ac23b5345c06c1287e02a4f5552f967dfe
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-20 13:26:59 +0200

    code refactoring.

--- backend/tests/data_test.py
+++ backend/tests/data_test.py
@@ -3,43 +3,52 @@ from test_base import TestBase, tokens
 
 
 class DataTest(TestBase):
-    def test_import_csv(self):
-        # unexisting csv file
+    # * >> Import csv route
+    def import_unexisting_csv(self):
         response = self.client.get(
             f'/data/import/csv/{tokens.working_dir}/Datedasd.csv')
         json_data = response.get_json()
         self.assertBasics(response, json_data)
         self.assertFalse(json_data[tokens.success])
 
-        # existing csv file
+    def import_existing_csv(self):
         response = self.client.get(
             f'/data/import/csv/{tokens.working_dir}/Date.csv')
         json_data = response.get_json()
         self.assertBasics(response, json_data)
         self.assertTrue(json_data[tokens.success])
 
-    def test_export_csv(self):
-        # directory does not exist
+    def test_import_csv(self):
+        self.import_unexisting_csv()
+        self.import_existing_csv()
+
+    # * >> Export csv route
+    def export_csv_directory_does_not_exist(self):
         response = self.client.get(
             f'/data/export/csv/name/DateExported.csv/path/{tokens.working_dir}/non-existing/wdkjdjkjw')
         json_data = response.get_json()
         self.assertBasics(response, json_data)
         self.assertFalse(json_data[tokens.success])
 
-        # nothing to save
+    def export_csv_nothing_to_save(self):
         response = self.client.get(
             f'/data/export/csv/name/DateExported.csv/path/{tokens.working_dir}')
         json_data = response.get_json()
         self.assertBasics(response, json_data)
         self.assertFalse(json_data[tokens.success])
 
-        # save the data
+    def export_csv(self):
         self.readDataFrame()
         response = self.client.get(
             f'/data/export/csv/name/DateExported.csv/path/{tokens.working_dir}')
         json_data = response.get_json()
         self.assertBasics(response, json_data)
         self.assertTrue(json_data[tokens.success])
+
+    def test_export_csv(self):
+        self.export_csv_directory_does_not_exist()
+        self.export_csv_nothing_to_save()
+        self.export_csv()
 
 
 if __name__ == '__main__':
