724c42c51893d69553801958814d2a6f0bdb3b4f
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-20 13:47:22 +0200

    added tests for get rows route.

--- backend/tests/data_test.py
+++ backend/tests/data_test.py
@@ -1,5 +1,6 @@
 import unittest
 from test_base import TestBase, tokens
+import server_data as sv
 
 
 class DataTest(TestBase):
@@ -44,6 +45,47 @@ class DataTest(TestBase):
         json_data = response.get_json()
         self.assertBasics(response, json_data)
         self.assertTrue(json_data[tokens.success])
+
+    # * -----------------------------------------------------------------------------------------------------
+    # * >> Get rows between route
+    # * -----------------------------------------------------------------------------------------------------
+    def test_get_rows_range_not_ints(self):
+        self.readDataFrame()
+        response = self.client.get('/data/rows-between/dasd/fr')
+        json_data = response.get_json()
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
+
+    def test_get_rows_invalid_range(self):
+        self.readDataFrame()
+        response = self.client.get('/data/rows-between/10/5')
+        json_data = response.get_json()
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
+
+    def test_get_rows_neative_start(self):
+        self.readDataFrame()
+        response = self.client.get('/data/rows-between/-10/5')
+        json_data = response.get_json()
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
+
+    def test_get_rows_end_out_of_bounds(self):
+        self.readDataFrame()
+        response = self.client.get(
+            f'/data/rows-between/0/{sv.dataFrame.shape[0] + 1000}')
+        json_data = response.get_json()
+        self.assertBasics(response, json_data)
+        self.assertFalse(json_data[tokens.success])
+
+    def test_get_rows(self):
+        self.readDataFrame()
+        response = self.client.get('/data/rows-between/0/10')
+        json_data = response.get_json()
+        self.assertBasics(response, json_data)
+        self.assertTrue(json_data[tokens.success])
+        self.assertTrue('dataframe' in json_data,
+                        "Missing 'dataframe' key in json response")
 
 
 if __name__ == '__main__':
