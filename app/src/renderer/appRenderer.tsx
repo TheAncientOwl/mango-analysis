8f935df1e3a8626a57399fc3b3cf341d5492bd50
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-26 20:25:58 +0200

    disabled eslint for inDev func. (project hierarchy bug?)

--- app/src/renderer/appRenderer.tsx
+++ app/src/renderer/appRenderer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/no-unresolved
 import { inDev } from '@common/helpers';
 import Application from './components/Application';
 
