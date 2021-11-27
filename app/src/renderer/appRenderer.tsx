721f31a576975f542f2736a997f2e2169e18ac76
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-27 16:58:13 +0200

    removed css boilerplate, added css baseline.

--- app/src/renderer/appRenderer.tsx
+++ app/src/renderer/appRenderer.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-unresolved
 import { inDev } from '@common/helpers';
 import Application from './components/Application';
+import { CssBaseline } from '@mui/material';
 
 console.log('[Info] : Renderer execution started');
 
 // Render application in DOM
-ReactDOM.render(<Application />, document.getElementById('app'));
+ReactDOM.render(
+  <React.Fragment>
+    <CssBaseline />
+    <Application />
+  </React.Fragment>,
+  document.getElementById('app')
+);
 
 // Hot module replacement
 if (inDev() && module.hot) module.hot.accept();
