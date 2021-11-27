b3a2d7f188b29759e9b4d43b90181574a154c598
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-27 16:56:28 +0200

    added topbar.

--- app/src/renderer/components/Application.tsx
+++ app/src/renderer/components/Application.tsx
@@ -1,42 +1,13 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import './Application.css';
+import { TopBar } from './TopBar';
 
 const Application: React.FC = () => {
   return (
-    <div className='main'>
-      <h1>⚡ Hello world! ⚡</h1>
-
-      <div className='tech'>
-        🗲
-        {['Electron', 'React', 'Typescript', 'Webpack'].map((item, index) => {
-          return (
-            <React.Fragment key={index}>
-              <span className='part color-primary'>{item}</span>-
-            </React.Fragment>
-          );
-        })}
-        <span className='part color-secondary'>boilerplate</span>
-        🗲
-      </div>
-
-      <div className='info'>
-        <h4 className='info-header'>You can start by editing:</h4>
-        <ul className='files'>
-          {[
-            { file: '/src/main/app.ts', desc: 'electron entry point' },
-            { file: '/src/renderer/appRenderer.tsx', desc: 'react frontend entry point' },
-            { file: '/src/renderer/appPreload.ts', desc: 'preload script entry point' },
-          ].map((item, index) => (
-            <li key={index} className='file'>
-              <span className='li-file'>{item.file}</span>
-              <span className='li-separator'>»</span>
-              <span className='li-file-desc'>{item.desc}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <React.Fragment>
+      <TopBar title='data' />
+    </React.Fragment>
   );
 };
 
---
+++ app/src/renderer/components/TopBar.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+interface Props {
+  title: string;
+}
+
+const Spacer = () => <Box sx={{ flexGrow: 1 }} />;
+
+export const TopBar: React.FC<Props> = ({ title }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton size='medium' edge='start' color='inherit' aria-label='menu' sx={{ mr: 1 }}>
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant='h6' component='div' sx={{ mr: 2 }}>
+            DataMaster
+          </Typography>
+
+          <Spacer />
+
+          <Typography variant='body1' sx={{ textTransform: 'capitalize' }}>
+            {title}
+          </Typography>
+
+          <Spacer />
+
+          <IconButton size='medium' edge='start' color='inherit' aria-label='menu' sx={{ ml: 1 }}>
+            <SettingsIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
