0e07dc6a9b04a1377389c3c8de063f255bfaf995
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-28 18:31:04 +0200

    chenaged secondary.

--- app/src/renderer/config/theme.ts
+++ app/src/renderer/config/theme.ts
@@ -1,13 +1,13 @@
-import { green, orange } from '@mui/material/colors';
+import { amber, green } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: green[900],
+      main: green[800],
     },
     secondary: {
-      main: orange[200],
+      main: amber[300],
     },
   },
 });
