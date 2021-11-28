42ec5f26e3903bc9377963ed8f519a471abe8a80
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-11-28 23:59:08 +0200

    changed palette.

--- app/src/renderer/config/theme.ts
+++ app/src/renderer/config/theme.ts
@@ -1,12 +1,19 @@
+import { yellow, green, teal, lightBlue } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#3E64E0',
+      main: green[900],
     },
     secondary: {
-      main: '#E0B53F',
+      main: yellow[600],
+    },
+    success: {
+      main: teal[900],
+    },
+    info: {
+      main: lightBlue[500],
     },
   },
 });
