bd82552c09e5fb5947accd6d62feb6fcd086f1e4
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-12-01 22:38:58 +0200

    removed unused import.

--- app/src/renderer/components/routes/data/View.tsx
+++ app/src/renderer/components/routes/data/View.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Box } from '@mui/material';
 import { DataFrame, DataConfig } from '@renderer/components/DataFrame';
