71bd9f4bd4aa0d4ef4ca060d18de9f24912f8bef
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-12-01 17:53:28 +0200

    changed theme breakpoints.

--- app/src/renderer/config/theme.ts
+++ app/src/renderer/config/theme.ts
@@ -16,4 +16,20 @@ export const theme = createTheme({
       main: lightBlue[500],
     },
   },
+  breakpoints: {
+    values: {
+      xs: 700,
+      sm: 1366,
+      md: 1440,
+      lg: 1926,
+      xl: 2560,
+      xxl: 3840,
+    },
+  },
 });
+
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xxl: true;
+  }
+}
