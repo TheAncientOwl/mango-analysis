d3d03a93d2a77dc5d3d25e77bd923cb847d8f0ab
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-12-03 15:34:32 +0200

    moved api func body into exposeInMainWorld func.

--- app/src/renderer/appPreload.tsx
+++ app/src/renderer/appPreload.tsx
@@ -1,45 +1,31 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { dialog } from '@electron/remote';
 
-const showOpenCsvDialog = async (): Promise<string | null> => {
-  const value = await dialog.showOpenDialog({
-    properties: ['openFile'],
-    filters: [{ name: 'Csv', extensions: ['csv'] }],
-  });
-
-  if (value.canceled) return null;
-
-  return value.filePaths[0];
-};
-
-const showOpenDirectoryDialog = async (): Promise<string | null> => {
-  const value = await dialog.showOpenDialog({
-    properties: ['openDirectory'],
-  });
-
-  if (value.canceled) return null;
-
-  return value.filePaths[0];
-};
-
-const minimizeAppWindow = () => {
-  ipcRenderer.invoke('window-minimize');
-};
-
-const toggleMaximizeAppWindow = () => {
-  ipcRenderer.invoke('window-toggle-maximize');
-};
-
-const closeAppWindow = () => {
-  ipcRenderer.invoke('window-close');
-};
-
 contextBridge.exposeInMainWorld('electron', {
-  showOpenCsvDialog,
-  showOpenDirectoryDialog,
-  minimizeAppWindow,
-  toggleMaximizeAppWindow,
-  closeAppWindow,
+  showOpenCsvDialog: async (): Promise<string | null> => {
+    const value = await dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [{ name: 'Csv', extensions: ['csv'] }],
+    });
+
+    if (value.canceled) return null;
+
+    return value.filePaths[0];
+  },
+
+  showOpenDirectoryDialog: async (): Promise<string | null> => {
+    const value = await dialog.showOpenDialog({
+      properties: ['openDirectory'],
+    });
+
+    if (value.canceled) return null;
+
+    return value.filePaths[0];
+  },
+
+  minimizeAppWindow: () => ipcRenderer.invoke('window-minimize'),
+  toggleMaximizeAppWindow: () => ipcRenderer.invoke('window-toggle-maximize'),
+  closeAppWindow: () => ipcRenderer.invoke('window-close'),
 });
 
 export {};
