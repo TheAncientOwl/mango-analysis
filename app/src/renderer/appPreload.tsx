4375b14f4eba2e3ee4256bc8c928df979b219dcf
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-12-03 16:07:33 +0200

    code refactoring.

--- app/src/renderer/appPreload.tsx
+++ app/src/renderer/appPreload.tsx
@@ -1,23 +1,28 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld('electron', {
-  getImportCsvPath: (): Promise<string | null> => ipcRenderer.invoke('get-import-csv-path'),
+interface ElectronAPI {
+  getImportCsvPath: () => Promise<string | null>;
+  getExportCsvDirectoryPath: () => Promise<string | null>;
+  minimizeAppWindow: () => void;
+  toggleMaximizeAppWindow: () => void;
+  closeAppWindow: () => void;
+}
+
+const electron: ElectronAPI = {
+  getImportCsvPath: () => ipcRenderer.invoke('get-import-csv-path'),
   getExportCsvDirectoryPath: () => ipcRenderer.invoke('get-export-csv-directory-path'),
 
   minimizeAppWindow: () => ipcRenderer.invoke('window-minimize'),
   toggleMaximizeAppWindow: () => ipcRenderer.invoke('window-toggle-maximize'),
   closeAppWindow: () => ipcRenderer.invoke('window-close'),
-});
+};
 
-export {};
+contextBridge.exposeInMainWorld('electron', electron);
+
 declare global {
   interface Window {
-    electron: {
-      getImportCsvPath: () => Promise<string | null>;
-      getExportCsvDirectoryPath: () => Promise<string | null>;
-      minimizeAppWindow: () => void;
-      toggleMaximizeAppWindow: () => void;
-      closeAppWindow: () => void;
-    };
+    electron: ElectronAPI;
   }
 }
+
+export {};
