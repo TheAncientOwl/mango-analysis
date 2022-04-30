70802148a379f1ade3d7e85f3eb28436b3e802dc
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-04-30 16:24:19 +0300

    more tests

--- server/tests/pca_test.py
+++ server/tests/pca_test.py
@@ -3,14 +3,18 @@ import main.server as server
 
 
 class PCA_Test(TestBase):
-    # -------------------------------------------------------------------------------------------------------
-    def test_get_targets_and_features(self):
-        self.readDataFrame()
-
+    def create_targets_features(self):
         targets = ['Country']
         features = list(server.dataFrame.columns)
         features.remove('Country')
         features.remove('_mango_id')
+
+        return [targets, features]
+
+    # -------------------------------------------------------------------------------------------------------
+    def test_get_targets_and_features(self):
+        self.readDataFrame()
+        targets, features = self.create_targets_features()
 
         response = self.client.get('/pca/possible/targets&features')
 
@@ -25,3 +29,17 @@ class PCA_Test(TestBase):
                         == targets, 'Incorrect targets')
         self.assertTrue(response_json['features']
                         == features, 'Incorrect features')
+
+    # -------------------------------------------------------------------------------------------------------
+    def test_scale_data(self):
+        self.readDataFrame()
+        targets, features = self.create_targets_features()
+
+        server.pca.set_target('Country')
+        server.pca.set_features(features)
+
+        response = self.client.post('/pca/scale-data')
+
+        self.assertStatusCodeCorrect(response, 200)
+        self.assertContentTypeJSON(response)
+        self.assertHasKey(response, 'message')
