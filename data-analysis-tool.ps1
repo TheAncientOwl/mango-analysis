e34475658834c8940677a416163183f84fd35ba7
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-05-07 14:52:36 +0300

    added terminal open shortcut script

---
+++ data-analysis-tool.ps1
@@ -0,0 +1,1 @@
+wt new-tab -p "Terminal" --title "mango" -d C:\Users\TheAncientOwl\Code\data-analysis-tool `; new-tab -p "Terminal" --title "mango-frontend" -d C:\Users\TheAncientOwl\Code\data-analysis-tool\app `; new-tab -p "Terminal" --title "mango-backend" -d C:\Users\TheAncientOwl\Code\data-analysis-tool\server `; focus-tab -t 0
