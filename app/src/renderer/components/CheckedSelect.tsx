08e2b2cb7b79c94a4db2ae0d83ca0583685b6759
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-05-10 21:46:25 +0300

    added checked select

---
+++ app/src/renderer/components/CheckedSelect.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  // eslint-disable-next-line import/named
+  SelectChangeEvent,
+} from '@mui/material';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      minWidth: 350,
+    },
+  },
+};
+
+interface Props {
+  maxWidth?: string;
+  minWidth?: string;
+  id: string;
+  label: string;
+  allChecked: boolean;
+  onCheckAll: () => void;
+  checkedValues: string[];
+  possibleValues: string[];
+  onChange: (event: SelectChangeEvent<string[]>) => void;
+}
+
+export const CheckedSelect: React.FC<Props> = ({
+  minWidth = 'auto',
+  maxWidth = 'auto',
+  id,
+  label,
+  allChecked,
+  onCheckAll,
+  checkedValues,
+  possibleValues,
+  onChange,
+}) => {
+  return (
+    <>
+      <Button
+        size='medium'
+        startIcon={allChecked ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon />}
+        onClick={onCheckAll}>
+        all
+      </Button>
+
+      <Box minWidth={minWidth} maxWidth={maxWidth}>
+        <FormControl fullWidth>
+          <InputLabel id={`${id}-label`}>{label}</InputLabel>
+          <Select
+            labelId={`${id}-label`}
+            id={`${id}`}
+            multiple
+            value={checkedValues}
+            label={`Display ${label}`}
+            onChange={onChange}
+            input={<OutlinedInput label='Tag' />}
+            renderValue={selected => selected.join(', ')}
+            MenuProps={MenuProps}>
+            {possibleValues.map(value => (
+              <MenuItem key={value} value={value}>
+                <Checkbox checked={checkedValues.indexOf(value) > -1} />
+                <ListItemText primary={value} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </>
+  );
+};
--- app/src/renderer/modules/principal-components-analysis/TargetAndFeaturesPicker.tsx
+++ app/src/renderer/modules/principal-components-analysis/TargetAndFeaturesPicker.tsx
@@ -11,36 +11,14 @@ import {
   lockNextStep,
 } from '@store/principal-components-analysis/actions';
 
-import {
-  Button,
-  Checkbox,
-  FormControl,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  // eslint-disable-next-line import/named
-  SelectChangeEvent,
-  Stack,
-} from '@mui/material';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+// eslint-disable-next-line import/named
+import { SelectChangeEvent, Stack } from '@mui/material';
 
+import { Select } from '@components/Select';
+import { CheckedSelect } from '@components/CheckedSelect';
 import { AnalysisStepLogic } from '@components/analysis-step';
 
 import { ComponentIndexPCA } from './ComponentIndexPCA';
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      minWidth: 200,
-    },
-  },
-};
 
 const VerticalLine = <Stack sx={{ m: 1, bgcolor: 'grey.700', p: 0.1 }}></Stack>;
 
@@ -70,71 +48,32 @@ const TargetAndFeaturesPicker: React.FC<PropsFromRedux> = props => {
       props.possibleValues.features.length === props.features.length ? [] : props.possibleValues.features
     );
 
-  // components
-  const selectTarget = (
-    <FormControl sx={{ minWidth: '6em' }}>
-      <InputLabel id='select-target-label'>Target</InputLabel>
-      <Select
-        labelId='select-target-label'
-        id='select-target'
-        value={props.possibleValues.targets?.length > 0 ? props.target : ''}
-        label='Display Targets'
-        onChange={handleTargetChange}>
-        {props.possibleValues.targets.map(target => (
-          <MenuItem key={target} value={target}>
-            {target}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-  );
-
-  const selectFeatures = (
-    <>
-      <Button
-        size='medium'
-        startIcon={
-          props.possibleValues.features.length > 0 && props.possibleValues.features.length === props.features.length ? (
-            <CheckBoxIcon />
-          ) : (
-            <CheckBoxOutlineBlankIcon />
-          )
-        }
-        onClick={handleSelectAllClick}>
-        all
-      </Button>
-
-      <FormControl sx={{ minWidth: '10em' }}>
-        <InputLabel id='select-features-label'>Features</InputLabel>
-        <Select
-          labelId='select-features-label'
-          id='select-features'
-          multiple
-          value={Array.from(props.features)}
-          label='Display Features'
-          onChange={handleFeaturesChange}
-          input={<OutlinedInput label='Tag' />}
-          renderValue={selected => selected.join(', ')}
-          MenuProps={MenuProps}>
-          {props.possibleValues.features.map(feature => (
-            <MenuItem key={feature} value={feature}>
-              <Checkbox checked={props.features.indexOf(feature) > -1} />
-              <ListItemText primary={feature} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </>
-  );
-
   return (
     <AnalysisStepLogic>
       <Stack direction='row' gap={1}>
-        {selectTarget}
+        <Select
+          minWidth='6em'
+          id='select-target'
+          label='Target'
+          value={props.possibleValues.targets?.length > 0 ? props.target : ''}
+          values={props.possibleValues.targets}
+          onChange={handleTargetChange}
+        />
 
         {VerticalLine}
 
-        {selectFeatures}
+        <CheckedSelect
+          minWidth='15em'
+          id='select-features'
+          label='Features'
+          allChecked={
+            props.possibleValues.features.length > 0 && props.possibleValues.features.length === props.features.length
+          }
+          onCheckAll={handleSelectAllClick}
+          checkedValues={props.features}
+          possibleValues={props.possibleValues.features}
+          onChange={handleFeaturesChange}
+        />
       </Stack>
     </AnalysisStepLogic>
   );
