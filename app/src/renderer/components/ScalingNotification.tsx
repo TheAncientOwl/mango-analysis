86751ab6edc1cd82af1a79c698339078b367f823
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-05-17 13:50:25 +0300

    moved link outside icon button

--- app/src/renderer/components/ScalingNotification.tsx
+++ app/src/renderer/components/ScalingNotification.tsx
@@ -16,32 +16,32 @@ export const ScalingNotification: React.FC<Props> = ({ scaled }) => {
   return (
     <>
       {!scaled && (
-        <IconButton
-          sx={{
-            position: 'absolute',
-            left: 0,
-            bottom: 0,
+        <Link to='/'>
+          <IconButton
+            sx={{
+              position: 'absolute',
+              left: 0,
+              bottom: 0,
 
-            zIndex: theme.zIndex.tooltip,
+              zIndex: theme.zIndex.tooltip,
 
-            m: 1.5,
-            pb: 0.5,
-            borderRadius: theme.shape.borderRadius,
-            border: '2px solid',
-            borderColor: theme.palette.error.main,
-            backgroundColor: alpha(theme.palette.common.black, 0.7),
-            ':hover': {
-              backgroundColor: alpha(theme.palette.common.black, 0.9),
-            },
-          }}>
-          <Link to='/'>
+              m: 1.5,
+              pb: 0.5,
+              borderRadius: theme.shape.borderRadius,
+              border: '2px solid',
+              borderColor: theme.palette.error.main,
+              backgroundColor: alpha(theme.palette.common.black, 0.7),
+              ':hover': {
+                backgroundColor: alpha(theme.palette.common.black, 0.9),
+              },
+            }}>
             <Tooltip title='For better results, scale the data! Click and go to the DataManager'>
               <Box color='error.main' className='blink'>
                 <PriorityHighIcon sx={{ fontSize: '2.5rem', cursor: 'pointer' }} />
               </Box>
             </Tooltip>
-          </Link>
-        </IconButton>
+          </IconButton>
+        </Link>
       )}
     </>
   );
