7d13ec25989aa171a978e57491fd30999f252516
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-05-23 16:00:58 +0300

    statistical tests

--- app/src/renderer/modules/factor-analysis/components/StatisticalHypotesisTesting.tsx
+++ app/src/renderer/modules/factor-analysis/components/StatisticalHypotesisTesting.tsx
@@ -3,22 +3,43 @@ import React from 'react';
 // eslint-disable-next-line import/named
 import { connect, ConnectedProps } from 'react-redux';
 import { RootState } from '@store/.';
-import { fetchBartlett, fetchKMO } from '@store/factor-analysis/actions';
+import { fetchBartlett, fetchKMO, unlockNextStep, lockNextStep } from '@store/factor-analysis/actions';
 
 import { Grid } from '@mui/material';
 
 import { AnalysisStepLogic } from '@components/analysis-step';
 import { StatisticalTest } from './StatisticalTest';
 
+import { ComponentsID } from '../config/componentsID';
+
 const StatisticalHypotesisTesting: React.FC<PropsFromRedux> = props => {
+  React.useEffect(() => {
+    if (props.bartlett.chiSquare !== undefined && props.kmoModel !== undefined)
+      props.unlockNextStep(ComponentsID.StatisticalHypotesisTesting);
+    else props.lockNextStep(ComponentsID.StatisticalHypotesisTesting);
+  }, [props.bartlett, props.kmoModel]);
+
   return (
     <AnalysisStepLogic>
       <Grid container gap={2}>
         <Grid item xs={5}>
-          <StatisticalTest title='Bartlett' tooltip='some tooltip'></StatisticalTest>
+          <StatisticalTest
+            onTest={props.fetchBartlett}
+            title='Bartlett'
+            tooltip='some tooltip'
+            values={[
+              { symbol: 'chi^2', value: props.bartlett.chiSquare, tooltip: 'some tooltip' },
+              { symbol: 'pValue', value: props.bartlett.pValue, tooltip: 'some tooltip' },
+            ]}
+          />
         </Grid>
-        <Grid item xs={6}>
-          <StatisticalTest title='KMO' tooltip='some tooltip'></StatisticalTest>
+        <Grid item xs={5}>
+          <StatisticalTest
+            onTest={props.fetchKMO}
+            title='KMO'
+            tooltip='some tooltip'
+            values={[{ symbol: 'kmoModel', value: props.kmoModel, tooltip: 'some tooltip' }]}
+          />
         </Grid>
       </Grid>
     </AnalysisStepLogic>
@@ -34,6 +55,8 @@ const mapState = (state: RootState) => ({
 const mapDispatch = {
   fetchBartlett,
   fetchKMO,
+  unlockNextStep,
+  lockNextStep,
 };
 
 const connector = connect(mapState, mapDispatch);
--- app/src/renderer/modules/factor-analysis/components/StatisticalTest.tsx
+++ app/src/renderer/modules/factor-analysis/components/StatisticalTest.tsx
@@ -1,20 +1,73 @@
 import React from 'react';
 
-import { Box, Typography, Stack } from '@mui/material';
+import { Box, Typography, Stack, Badge, Button, Collapse } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import BoltIcon from '@mui/icons-material/Bolt';
 
+import { Tooltip } from '@components/Tooltip';
 import { Paper } from '@components/Paper';
+
+interface TestValueConfig {
+  symbol: string;
+  value: number;
+  tooltip: string;
+}
 
 interface Props {
   title: string;
   tooltip?: string;
+  values: TestValueConfig[];
+  onTest: () => void;
 }
 
-export const StatisticalTest: React.FC<Props> = ({ title, tooltip, children }) => {
+const TestValue: React.FC<TestValueConfig> = ({ symbol, value, tooltip }) => {
   return (
-    <Paper>
-      <Box sx={{ border: '1px solid green', flexGrow: '1' }}>
-        <Typography variant='body1'>{title}</Typography>
+    <Box>
+      <Badge
+        sx={{ mr: 2 }}
+        badgeContent={
+          <Tooltip title={tooltip} placement='right'>
+            <InfoIcon sx={{ color: 'info.main', fontSize: '15px', ml: 2 }} />
+          </Tooltip>
+        }>
+        {symbol}
+      </Badge>
+      {' = '}
+      {value}
+    </Box>
+  );
+};
+
+export const StatisticalTest: React.FC<Props> = ({ title, tooltip, values, onTest }) => {
+  return (
+    <Paper sx={{ display: 'block', p: 2 }}>
+      <Box>
+        <Stack direction='row' gap={1} pl={1} pr={2} justifyContent='space-between' alignItems='center'>
+          <Badge
+            badgeContent={
+              <Tooltip title={tooltip} placement='right'>
+                <InfoIcon sx={{ color: 'info.main', fontSize: '17px', ml: 2 }} />
+              </Tooltip>
+            }>
+            <Typography variant='body1' pl={2}>
+              {title}
+            </Typography>
+          </Badge>
+
+          <Button startIcon={<BoltIcon />} sx={{ ml: 2 }} size='small' onClick={onTest}>
+            run test
+          </Button>
+        </Stack>
+
         <Stack sx={{ m: 1, bgcolor: 'grey.700', p: 0.1 }}></Stack>
+
+        <Collapse in={values[0].value !== undefined}>
+          <Stack direction='column' gap={1} pl={2} mt={2}>
+            {values.map((value, index) => (
+              <TestValue key={index} symbol={value.symbol} value={value.value} tooltip={value.tooltip} />
+            ))}
+          </Stack>
+        </Collapse>
       </Box>
     </Paper>
   );
