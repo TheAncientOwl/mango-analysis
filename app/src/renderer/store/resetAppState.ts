be4f1bbe4f0f5ca900da1a506aed25a84c02d89e
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-06-08 12:53:51 +0300

    .

--- app/src/renderer/store/resetAppState.ts
+++ app/src/renderer/store/resetAppState.ts
@@ -1,12 +1,12 @@
-import { store } from '.';
-import { resetState as resetStatePCA } from './principal-components-analysis/actions';
-import { resetState as resetStateFactorAnalysis } from './factor-analysis/actions';
-import { resetState as resetStateLinearRegression } from './linear-regression/actions';
 import { resetState as resetStateMultipleLinearRegression } from './multiple-linear-regression/actions';
+import { resetState as resetStateLinearRegression } from './linear-regression/actions';
+import { resetState as resetStateFactorAnalysis } from './factor-analysis/actions';
+import { resetState as resetStatePCA } from './principal-components-analysis/actions';
+import { store } from '.';
 
 export const resetAppState = () => {
-  store.dispatch(resetStatePCA());
-  store.dispatch(resetStateFactorAnalysis());
-  store.dispatch(resetStateLinearRegression());
   store.dispatch(resetStateMultipleLinearRegression());
+  store.dispatch(resetStateLinearRegression());
+  store.dispatch(resetStateFactorAnalysis());
+  store.dispatch(resetStatePCA());
 };
