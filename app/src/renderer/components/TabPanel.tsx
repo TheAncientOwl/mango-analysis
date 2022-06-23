cff33f17ea281ff09eb342445d94ef15112f6adb
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-06-23 15:12:40 +0300

    removed typography component

--- app/src/renderer/components/TabPanel.tsx
+++ app/src/renderer/components/TabPanel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Typography, Box } from '@mui/material';
+import { Box } from '@mui/material';
 
 interface Props {
   children?: React.ReactNode;
@@ -10,10 +10,6 @@ interface Props {
 
 export const TabPanel: React.FC<Props> = ({ children, value, index, ...other }) => (
   <div role='tabpanel' hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} {...other}>
-    {value === index && (
-      <Box sx={{ p: 3 }}>
-        <Typography>{children}</Typography>
-      </Box>
-    )}
+    {value === index && <Box p={3}>{children}</Box>}
   </div>
 );
